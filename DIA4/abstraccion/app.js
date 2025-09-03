6b10b8af23b24a9f299a9ca75c926a959867a55d
Danny200523 <danicrack0709@gmail.com>
2025-09-03 12:59:49 -0500

    FEAT: :zap: UPDATE CODE

---
+++ DIA4/abstraccion/app.js
@@ -0,0 +1,15 @@
+/*
+--ABSTRACCION--
+
+se consistes en mostrar solo lo esencial y ocultar lo complejo
+se logra creando metodos que represnetan acciones de oalto nivel sin necesidad que el usuario entienda toda la logica interna. */
+
+class Coche{
+      encender(){
+        console.log(`el coche esta encendido`);
+      }
+      apagar(){
+        console.log(`el coche esta encendido`);
+      }
+}
+
